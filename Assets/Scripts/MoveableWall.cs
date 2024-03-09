ae2cf87faed6e84dda21b49c2eaf675f127157e7
Ahmad Ali <ahmadshykh2015@gmail.com>
2024-03-10 02:33:16 +0500

    Moveable and Static Walls Done

---
+++ Assets/Scripts/MoveableWall.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MoveableWall : Walls
+{
+	[Header("Wall Moving Configrations")]
+	[SerializeField] float _wallSpeed;
+	[SerializeField] float _minZValue;
+	[SerializeField] float _maxZValue;
+
+    public MoveableWall()
+    {
+		
+    }
+
+    void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit raycastHit;
+
+			if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+			{
+				if(raycastHit.collider.gameObject == gameObject &&raycastHit.point.z > _minZValue && raycastHit.point.z < _maxZValue) {
+					transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, raycastHit.point.z), _wallSpeed * Time.deltaTime);
+				}
+			}
+		}
+	}
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Bullet")
+		{
+			Destroy(collider.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Walls.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements.Experimental;
+
+public class Walls : MonoBehaviour
+{
+    
+}
+
+
+public enum WallType {
+    Moveable,
+    Static,
+    Bouncable
+}
