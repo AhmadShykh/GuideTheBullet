8d22330c7fcf4bc714a6cc38589179afe0505a8a
FarhanFK2003 <farhan.khanfk2003@gmail.com>
2024-03-10 10:25:26 +0500

    Added Scene Timer

---
+++ Assets/F_Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float totalTime = 180.0f; // Total countdown time in seconds
+    private float currentTime; // Current remaining time
+
+    public Text timerText; // Reference to the UI Text element
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set initial time
+        currentTime = totalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update current time
+        currentTime -= Time.deltaTime;
+
+        // Clamp current time to avoid negative values
+        if (currentTime < 0)
+        {
+            currentTime = 0;
+        }
+
+        // Calculate minutes and seconds
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+
+        // Update UI text with formatted time
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
